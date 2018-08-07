cff11cf6879b70c1900c2f0ae41ae7adb3c2fe96
Matt <mbach04@users.noreply.github.com>
2018-08-07 12:19:04 -0400

    Change listening port for https on OCP

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ function start(route, handle) {
         route(handle, pathname, response, request);
     }
 
-    http.createServer(onRequest).listen(8080);
+    http.createServer(onRequest).listen(8443);
 }
 
 exports.start = start;
